d772ff0af25415de3cc0ee454d2d7f3d68714b6f
Alan Mauricio Reyes Telesforo <mauricio.gobti@gmail.com>
2024-04-22 11:22:46 -0600

    cambio de contraseña

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -219,4 +219,44 @@ export class AuthController {
     static user = async (req: Request, res: Response) => {
         return res.json(req.user);
     }
+
+    static updateProfile = async (req: Request, res: Response) => {
+        const { name, email } = req.body;
+
+        const userExists = await User.findOne({ email });
+        if(userExists && userExists.id.toString() !== req.user.id.toString()) {
+            const error = new Error('El usuario ya esta registrado');
+            return res.status(404).json({ error: error.message });
+        }
+
+        req.user.name = name;
+        req.user.email = email;
+
+        try {
+            await req.user.save();
+            res.send('Perfil actualizado correctamente');
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' });
+        }
+    }
+
+    static updateCurrentUsersPassword = async (req: Request, res: Response) => {
+        const { current_password, password } = req.body;
+        
+        const user = await User.findById(req.user.id);
+
+        const isPaswordCorrect = await checkPassword(current_password, user.password);
+        if(!isPaswordCorrect) {
+            const error = new Error('Password actual incorrecto');
+            return res.status(401).json({ error: error.message });
+        }
+
+        try {
+            user.password = await hashPassword(password);
+            await user.save();
+            res.send('Password actualizado correctamente');
+        } catch (error) {
+            res.status(500).json({ error: 'Hubo un error' });
+        }
+    }
 }
--- src/routes/authRoutes.ts
+++ src/routes/authRoutes.ts
@@ -68,4 +68,28 @@ router.get('/user',
     authenticate,
     AuthController.user
 )
+
+/** PROFILE */
+router.put('/profile',
+    authenticate,
+    body('name').notEmpty().withMessage('El nombre es obligatorio'),
+    body('email').isEmail().withMessage('E-mail no valido'),
+    handleInputErrors,
+    AuthController.updateProfile
+)
+
+router.post('/update-password',
+    authenticate,
+    body('current_password').notEmpty().withMessage('El password actual es obligatorio'),
+    body('password').isLength({ min: 8 }).withMessage('El password es muy corto minimo 8 caracteres'),
+    body('password_confirmation').custom((value, {req}) => {
+        if(value !== req.body.password) {
+            throw new Error('Los password no son iguales');
+        }
+        return true;
+    }),
+    handleInputErrors,
+    AuthController.updateCurrentUsersPassword
+)
+
 export default router;
